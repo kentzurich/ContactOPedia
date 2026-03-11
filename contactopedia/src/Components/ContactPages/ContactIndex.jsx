f1019c036d526085cbc2627b8e48209180b85c2e
Kent Zurich <kentzurichanthony@gmail.com>
2026-03-11 14:29:45 +0800

    UI Layout

--- contactopedia/src/Components/ContactPages/ContactIndex.jsx
+++ contactopedia/src/Components/ContactPages/ContactIndex.jsx
@@ -22,7 +22,28 @@ function ContactIndex() {
     },
   ]);
 
-  return <div>Hello</div>;
+  return (
+    <div className="container" style={{ minHeight: "85vh" }}>
+      <div className="py-3">
+        <div className="row py-2">
+          <div className="col-6">ADD CONTACT</div>
+          <div className="col-6">REMOVE CONTACT</div>
+        </div>
+
+        <div className="py-2">
+          <div className="col-12">FORM TO ADD NEW CONTACT</div>
+        </div>
+
+        <div className="py-2">
+          <div className="col-12">FAVORITES CONTACT</div>
+        </div>
+
+        <div className="py-2">
+          <div className="col-12">GENERAL CONTACT</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactIndex;
